3c8463e1805b7d4b1b871a10e753e24c465b345d
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2025-09-13 14:40:47 +0530

    update hashing

---
+++ HashTables/CharacterHashing.java
@@ -0,0 +1,29 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class CharacterHashing {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("input.txt");
+        Scanner sc = new Scanner(file);
+
+        String s = sc.nextLine();
+
+        // precompute
+        int hash[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)-'a'] += 1;
+        }
+
+        int q = sc.nextInt();
+
+        while (q > 0) {
+            char c = sc.next().charAt(0); 
+            System.out.println(hash[c-'a']);
+            q--;
+        }
+    }
+}
