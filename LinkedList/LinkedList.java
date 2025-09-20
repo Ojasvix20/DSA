a7fa83eab17c5408f049c10196444711f098a85d
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2025-09-20 15:30:57 +0530

    add LL files

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,37 @@
+
+class LinkedList {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 7};
+
+        Node head = new Node(arr[0]);
+        Node curr = head;
+        int j = 1;
+        while (j < arr.length) {
+            Node temp = new Node(arr[j]);
+            curr.next = temp;
+            curr = temp;
+            j++;
+        }
+        Node p = head;
+        while (p != null) {
+            if (p.next != null) {
+                System.out.print(p.data + "->");
+            } else {
+                System.out.print(p.data);
+            }
+            p = p.next;
+        }
+    }
+}
