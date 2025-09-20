7f318c4c99687a8d76d59e8f089d0add50b16560
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2025-09-21 00:50:07 +0530

    update LL

---
+++ LinkedList/DeletionInLL.java
@@ -0,0 +1,84 @@
+
+public class DeletionInLL {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node arrayToLL(int[] arr) {      //convert array to 1D Linked List
+        Node head = new Node(arr[0]);
+        Node curr = head;
+        int j = 1;
+        while (j < arr.length) {
+            Node temp = new Node(arr[j]);
+            curr.next = temp;
+            curr = temp;
+            j++;
+        }
+        return head;
+    }
+
+    static void printLL(Node head) {
+        Node p = head;
+        while (p != null) {
+            if (p.next != null) {
+                System.out.print(p.data + "->");
+            } else {
+                System.out.print(p.data);
+            }
+            p = p.next;
+        }
+    }
+
+    static Node DeleteHead(Node head) {
+        if (head == null) {
+            return head;
+        }
+        head = head.next; //java has garbage collector unline C++ so we dont manually have to delete old head
+        return head;
+    }
+
+    static Node DeleteTail(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        Node tail = temp.next;
+        temp.next = null;   //delete tail in LL
+        return tail;
+
+    }
+
+    static Node DeleteAtPos(Node head, int k) {
+        if (head == null) {
+            return null;
+        }
+        if (k == 1) {
+            head = head.next;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 7};
+        Node head = arrayToLL(arr);
+
+        //delete head
+        // System.out.println(DeleteHead(head).data);
+        //delete tail
+        // System.out.println("tail removed: " + DeleteTail(head).data);
+        // printLL(head);
+        //delete At Position
+    }
+}
--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,4 +1,4 @@
-
+// includes arrayToLL, search in LL, print LL, get length of LL
 class LinkedList {
 
     static class Node {
@@ -12,9 +12,7 @@ class LinkedList {
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {2, 5, 8, 7};
-
+    static Node arrayToLL(int[] arr) {      //convert array to 1D Linked List
         Node head = new Node(arr[0]);
         Node curr = head;
         int j = 1;
@@ -24,14 +22,51 @@ class LinkedList {
             curr = temp;
             j++;
         }
+        return head;
+    }
+
+    static int lengthOfLL(Node head) {      //fetch the length of the linked list
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    static Boolean SearchInLL(Node head, int val) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == val) {
+                return true;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 7};
+
+        Node head = arrayToLL(arr);
+
+        // Printing LL
         Node p = head;
         while (p != null) {
             if (p.next != null) {
                 System.out.print(p.data + "->");
             } else {
-                System.out.print(p.data);
+                System.out.println(p.data);
             }
             p = p.next;
         }
+
+        // printing LL length:
+        System.out.println("Length of Linked List: " + lengthOfLL(head));
+
+        System.out.println(SearchInLL(head, 4));
+        System.out.println(SearchInLL(head, 7));
     }
 }
