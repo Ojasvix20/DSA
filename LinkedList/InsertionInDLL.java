43914ae3ce3d0cb3c14d2b9cd81de4784b8c5b36
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2025-09-29 12:41:42 +0530

    update LL

--- LinkedList/InsertionInDLL.java
+++ LinkedList/InsertionInDLL.java
@@ -27,12 +27,12 @@ public class InsertionInDLL {
 
     static Node ArrayToLL(int[] arr) {
         Node head = new Node(arr[0]);
-        Node temp = head;
+        Node tailPrev = head;
         for (int i = 1; i < arr.length; i++) {
             Node newNode = new Node(arr[i]);
-            temp.next = newNode;
-            newNode.prev = temp;
-            temp = newNode;
+            tailPrev.next = newNode;
+            newNode.prev = tailPrev;
+            tailPrev = newNode;
         }
         return head;
     }
@@ -51,12 +51,104 @@ public class InsertionInDLL {
         return newHead;
     }
 
+    static Node InsertBeforeTail(Node head, int val) {
+        if (head == null) {
+            return head;
+        }
+        Node newNode = new Node(val);
+        if (head.next == null) {
+            head.prev = newNode;
+            newNode.next = head;
+            head = newNode;
+            return head;
+        }
+
+        Node tailPrev = head;
+        while (tailPrev.next.next != null) {
+            tailPrev = tailPrev.next;
+        }
+        Node tail = tailPrev.next;
+        tailPrev.next = newNode;
+        newNode.next = tail;
+        newNode.prev = tailPrev;
+        tail.prev = newNode;
+
+        return head;
+
+    }
+
+    static Node InsertBeforePos(Node head, int pos, int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            if (pos == 1) {
+                return newNode;
+            } else {
+                return head; // invalid pos > 1 in empty list
+            }
+        }
+        if (pos <= 1) {
+            newNode.next = head;
+            head.prev = newNode;
+            return newNode; // new head
+        }
+
+        int i = 1;
+        Node temp = head;
+        while (i < pos - 1 && temp.next != null) {
+            temp = temp.next;
+            i++;
+        }
+
+        if (temp.next == null) {
+            // means pos was out of bounds → insert at end (optional)
+            temp.next = newNode;
+            newNode.prev = temp;
+            return head;
+        }
+        Node NodeAtPos = temp.next;
+        temp.next = newNode;
+        newNode.prev = temp;
+        newNode.next = NodeAtPos;
+        NodeAtPos.prev = newNode;
+
+        return head;
+    }
+
+    static Node InsertBeforeNode(Node head, int val, Node n) {
+        Node newNode = new Node(val);
+        if (n == null) {
+            return head;
+        }
+
+        if (n == head) {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+            return head;
+        }
+
+        Node nPrev = n.prev;
+        n.prev = newNode;
+        newNode.next = n;
+        nPrev.next = newNode;
+        newNode.prev = nPrev;
+
+        return head;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6 };
         Node head = ArrayToLL(arr);
         printLL(head);
 
-        head = InsertBeforeHead(head, 69);
+        // head = InsertBeforeHead(head, 69);
+        // printLL(head);
+        // head = InsertBeforeTail(head, 69);
+        // printLL(head);
+        // head = InsertBeforePos(head, 3, 69);
+        // printLL(head);
+        Node n = head.next.next.next;
+        head = InsertBeforeNode(head, 69, n);
         printLL(head);
     }
 }
