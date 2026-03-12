bc03698422a4e85d4a33863ee31b92d4facd7113
Ojasvi Bakshi <ojasvi.bakshi@gmail.com>
2026-03-13 00:19:51 +0530

    .

--- SegmentTree/ValidBrackets.java
+++ SegmentTree/ValidBrackets.java
@@ -1,27 +1,52 @@
 //code forces 380c
-class Node{
-   char data;
-   int opening, valid,closing; 
-   Node(char data, int opening, int closing,int valid){
-    this.data= data;
-    this.opening=opening;
-    this.closing= closing;
-    this.valid=valid;
-   }
-}
-public class ValidBrackets{
 
-    public static Node merge(Node left, Node right){
+// Node(int opening, int closing, int valid) {
+
+//     this.opening = opening;
+//     this.closing = closing;
+//     this.valid = valid;
+// }
+
+import java.util.*;
+
+public class ValidBrackets {
+
+    static Node[] seg;
+    static char[] arr;
+
+    static class Node {
+        int opening, valid, closing;
+    }
+
+    public static Node merge(Node left, Node right) {
         Node res = new Node();
-        int matched = Math.min(a.opening, b.closing);
-        res.valid = a.valid+ b.valid + MATCHED;
-        res.opening = a.opening + b.opening  - matched;
-        res.closing = a.closing + b.closing  - matched;
+        int matched = Math.min(left.opening, right.closing);
+        res.valid = left.valid + right.valid + matched;
+        res.opening = left.opening + right.opening - matched;
+        res.closing = left.closing + right.closing - matched;
 
         return res;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        String s =  sc.next();
+        arr=  s.toCharArray();
+        int n= s.length();
+        int q = sc.nextInt();
+        while(q-->0){
+            int ql = sc.nextInt();
+            int qr = sc.nextInt();
+            ql--; qr--;
 
+            query(0,0,n-1,ql,qr);
+        }
+
+
+
+        sc.close();
+    }
+    public static Node query(int idx, int l, int r, int ql, int qr){
+        
     }
 }
